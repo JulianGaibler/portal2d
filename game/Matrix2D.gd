a205a9a6e9adbbc38f2f697c5e7ea04a5f9fbc61
JulianWels <julian@jwels.berlin>
2019-10-30 22:42:32 +0100

    :construction: Implemented portal position and velocity transformation

---
+++ game/Matrix2D.gd
@@ -0,0 +1,34 @@
+class_name Matrix2D
+
+# Matrix structure:
+# ( x.x x.y )
+# ( y.x y.y )
+
+var x: Vector2
+var y: Vector2
+
+func _init(xx: float, xy: float, yx: float, yy: float):
+    self.x = Vector2(xx, xy)
+    self.y = Vector2(yx, yy)
+
+func inverse():
+    var mat = get_script().new(self.y.y, -self.x.y, -self.y.x, self.x.x)
+    var num = 1 / (self.x.x * self.y.y - self.x.y * self.y.x)
+    return mat.multiply_num(num)
+
+func multiply_mat(other: Matrix2D) -> Matrix2D:
+    return get_script().new(
+        self.x.x * other.x.x + self.x.y * other.y.x,
+        self.x.x * other.x.y + self.x.y * other.y.y,
+        self.y.x * other.x.x + self.y.y * other.y.x,
+        self.y.x * other.x.y + self.y.y * other.y.y)
+
+func multiply_vec(other: Vector2):
+    return Vector2(self.x.x * other.x + self.x.y * other.y,
+                   self.y.x * other.x + self.y.y * other.y)
+
+func multiply_num(scalar: float):
+    return get_script().new(self.x.x * scalar, self.x.y * scalar, self.y.x * scalar, self.y.y * scalar)
+
+func print() -> String:
+    return "{ {%2.2f, %2.2f} {%2.2f, %2.2f} }" % [self.x.x, self.x.y, self.y.x, self.y.y]
--- game/PolygonUtils.gd
+++ game/PolygonUtils.gd
@@ -2,34 +2,34 @@ class_name PolygonUtils
 
 # Transforms polygon with given Transform2D
 static func transform_polygon(polygon: PoolVector2Array, tf: Transform2D) -> PoolVector2Array:
-	var newVec = PoolVector2Array()
-	newVec.resize(polygon.size())
-	for i in range(0, polygon.size()):
-		newVec.set(i, tf.xform(polygon[i]))
-	return newVec
+    var newVec = PoolVector2Array()
+    newVec.resize(polygon.size())
+    for i in range(0, polygon.size()):
+        newVec.set(i, tf.xform(polygon[i]))
+    return newVec
 
 
 # Converts shapes into polygons
 # (Everything besides rectangles and polygons will return null)
 static func shape_to_polygon(shape_rid: RID): # -> PoolVector2Array
-	var polygon: PoolVector2Array
+    var polygon: PoolVector2Array
 
-	var shape_type = Physics2DServer.shape_get_type(shape_rid)
-	var shape_data = Physics2DServer.shape_get_data(shape_rid)
-	match shape_type:
-		
-		Physics2DServer.SHAPE_RECTANGLE:
-			polygon = PoolVector2Array([
-				Vector2(-shape_data.x, shape_data.y),
-				Vector2(-shape_data.x, -shape_data.y),
-				Vector2(shape_data.x, -shape_data.y),
-				Vector2(shape_data.x, shape_data.y),
-			])
-			
-		Physics2DServer.SHAPE_CONVEX_POLYGON, Physics2DServer.SHAPE_CUSTOM:
-			polygon = PoolVector2Array(shape_data)
-			
-		_:
-			return null
+    var shape_type = Physics2DServer.shape_get_type(shape_rid)
+    var shape_data = Physics2DServer.shape_get_data(shape_rid)
+    match shape_type:
+        
+        Physics2DServer.SHAPE_RECTANGLE:
+            polygon = PoolVector2Array([
+                Vector2(-shape_data.x, shape_data.y),
+                Vector2(-shape_data.x, -shape_data.y),
+                Vector2(shape_data.x, -shape_data.y),
+                Vector2(shape_data.x, shape_data.y),
+            ])
+            
+        Physics2DServer.SHAPE_CONVEX_POLYGON, Physics2DServer.SHAPE_CUSTOM:
+            polygon = PoolVector2Array(shape_data)
+            
+        _:
+            return null
 
-	return polygon
+    return polygon
