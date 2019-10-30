d17b67bf81129297c1b92a86231fe98c5c9ae185
JulianWels <julian@jwels.berlin>
2019-10-30 23:00:12 +0100

    :fire: Code cleanup

--- game/World.gd
+++ game/World.gd
@@ -2,20 +2,20 @@ extends Node2D
 
 export var blue_portal_hit_effect: PackedScene
 export var orange_portal_hit_effect: PackedScene
-	
+    
 func generate_blue_hit_effect(hit_position: Vector2) ->void:
-	var temp = blue_portal_hit_effect.instance()
-	add_child(temp)
-	temp.position = hit_position
+    var temp = blue_portal_hit_effect.instance()
+    add_child(temp)
+    temp.position = hit_position
 
 func generate_orange_hit_effect(hit_position: Vector2) ->void:
-	var temp = orange_portal_hit_effect.instance()
-	add_child(temp)
-	temp.position = hit_position
+    var temp = orange_portal_hit_effect.instance()
+    add_child(temp)
+    temp.position = hit_position
 
 func _on_Player_fired_blue_portal(hit_position: Vector2):
-	generate_blue_hit_effect(hit_position)
+    generate_blue_hit_effect(hit_position)
 
 
 func _on_Player_fired_orange_portal(hit_position: Vector2):
-	generate_orange_hit_effect(hit_position)
+    generate_orange_hit_effect(hit_position)
