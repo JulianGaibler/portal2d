b083c2387ce6ee2baa4e1fd07ee2353f7b5bd0e5
daehruoydeef <daeh@tuta.io>
2019-12-12 11:35:55 +0100

    added move_to_start, move_to_end

--- game/test-elements/moving-platform/MovingPlatform.gd
+++ game/test-elements/moving-platform/MovingPlatform.gd
@@ -7,25 +7,28 @@ extends Node2D
 # Don't worry, this will probably take less than an hour. Thanks for doing it!
 
 export(bool) var is_active = false
+export(bool) var autostart = true
 export(bool) var loop = false
-export var duration = 3
+export (bool) var move_to_start = false
+export (int) var duration = 3
 export var wait_duration = 1
 export (NodePath) var positions_points = NodePath()
+var positions = []
+var start_position
 onready var TweenNode = get_node("Tween")
 onready var DurationTimer = get_node("Timer")
 
-var DURATION = duration
-var WAIT_DURATION = wait_duration
 var waypoint_index = 1
 
 func _ready():
-    if is_active:
+    start_position = position
+    if autostart:
         start()
 
 func start():
     is_active = true
     var waypoints = get_node(positions_points).get_children()
-    var positions = []
+    positions = []    
     for i in range(len(waypoints) + 1):
         if i == 0:
             positions.append(position)
@@ -33,39 +36,54 @@ func start():
             positions.append(waypoints[i-1].position)
     if waypoint_index == 1:
         next_move(positions)
-    DurationTimer.set_wait_time(DURATION)
+    DurationTimer.set_wait_time(duration)
     DurationTimer.connect("timeout",self, "_on_Timer_timeout", [positions])
     DurationTimer.start()
+    
 
 func next_move(positions):
     if (waypoint_index == len(positions)):
+        positions.invert()
+        waypoint_index = 0
         if loop:
-            positions.invert()
-            waypoint_index = 1
+            is_active = true
         else:
-            DurationTimer.stop()
+            stop()
             return
     move(position, positions[waypoint_index])
     waypoint_index += 1
     
 func stop():
+    if move_to_start:
+        move_to_start()
     TweenNode.stop(TweenNode)
     DurationTimer.stop()
-    
+    is_active = false
+
+func move_to_start():
+    TweenNode.interpolate_property(self, "position", position, start_position, duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+    TweenNode.start()
+
+func move_to_end():
+    TweenNode.interpolate_property(self, "position", position, positions[len(positions)-1], duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+    TweenNode.start()    
+
 func move(from, to):
-    print(to)
-    TweenNode.interpolate_property(self, "position", from, to, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+    TweenNode.interpolate_property(self, "position", from, to, duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
     TweenNode.start()
 
 
-func _on_PedestalButton_pressed():
-    if (not is_active):
-        is_active = true
-        start()
+func toggle():
+    if not is_active:
+        activate()
     else:
-        is_active = false
-        stop()
+        deactivate()
         
+func deactivate():
+    stop()
+
+func activate():
+    start()
 
 func _on_Timer_timeout(positions):
     next_move(positions)
