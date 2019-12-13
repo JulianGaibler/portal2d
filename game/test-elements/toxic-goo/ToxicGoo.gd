9cf49c06d46e44462640e4106d541623bd80dde8
JulianWels <julian@jwels.berlin>
2019-12-13 20:27:20 +0100

    :sparkles: Added toxic goo (which does not kill the player yet because they cannot die)

---
+++ game/test-elements/toxic-goo/ToxicGoo.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+onready var goo = $GooSprite
+
+func _ready():
+    goo.material = goo.material.duplicate()
+    goo.material.set_shader_param("sprite_scale", global_scale * 5)
+    connect("body_entered", self, "enter_inner_area")
+
+func enter_inner_area(body):
+    if body.is_in_group("can-fizzle"):
+        body.fizzle()
+    elif body.is_in_group("player"):
+        print("PLAYA IS DED NAO!")
