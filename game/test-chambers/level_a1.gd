18861ae27109bcbc5c60f5a833f1dc1f93ae25f9
JulianWels <julian@jwels.berlin>
2020-01-23 22:14:58 +0100

    :sparkles: Tooltips

--- game/test-chambers/level_a1.gd
+++ game/test-chambers/level_a1.gd
@@ -4,10 +4,12 @@ onready var spawner_panel = $SpawnerPanel
 onready var spawner_orange = $PortalSpawnerOrange
 onready var spawner_blue = $SpawnerPanel/PanelTransform/PortalSpawnerBlue
 onready var sign_00 = $TestchamberSign00
+onready var tooltip = $Tooltip_1
 
 func _ready():
     yield(get_tree().create_timer(3), "timeout")
     sign_00.turn_on()
+    tooltip.show_message()
     yield(get_tree().create_timer(1), "timeout")
     spawner_panel.play_animation("corder-90-left-out-heavy")
     yield(get_tree().create_timer(4), "timeout")
